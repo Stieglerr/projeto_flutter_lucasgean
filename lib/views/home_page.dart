180329eaa8aa67b879b18914c41138d77178c735
Stieglerr <stieglerstr@hotmail.com>
2024-11-05 14:05:13 -0300

    segundo commit

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:new_login_screen/services/firestore_service.dart';
@@ -19,7 +20,14 @@ class _HomePageState extends State<HomePage> {
     FirebaseAuth.instance.signOut();
   }
 
-  void _openModalForm() {
+  void _openModalForm(String? docId) async {
+    if (docId != null) {
+      DocumentSnapshot documentSnapshot = await firestoreService.getTask(docId);
+      Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
+      _titleController.text = data["title"];
+      _descriptionController.text = data["description"];
+    }
+    
     showModalBottomSheet(
       backgroundColor: Color.fromARGB(255, 99, 131, 151),
       context: context,
@@ -44,16 +52,13 @@ class _HomePageState extends State<HomePage> {
                 "Adicionar Tarefas",
                 style: TextStyle(fontSize: 24, color: Colors.white),
               ),
-              Divider(
-                color: Colors.white,
-              ),
+              Divider(color: Colors.white),
               SizedBox(height: 10),
               TextField(
                 controller: _titleController,
                 decoration: InputDecoration(
                   labelText: "Título",
                   fillColor: Colors.white,
-                  focusColor: Colors.white,
                   filled: true,
                   prefixIcon: Icon(Icons.text_fields_outlined),
                   border: OutlineInputBorder(
@@ -66,10 +71,9 @@ class _HomePageState extends State<HomePage> {
                 controller: _descriptionController,
                 decoration: InputDecoration(
                   fillColor: Colors.white,
-                  focusColor: Colors.white,
                   filled: true,
                   labelText: 'Descrição',
-                  prefixIcon: Icon(Icons.person),
+                  prefixIcon: Icon(Icons.description),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
                   ),
@@ -78,10 +82,12 @@ class _HomePageState extends State<HomePage> {
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  firestoreService.addTask(
-                    _titleController.text, _descriptionController.text);               
-                  _titleController.clear();
-                  _descriptionController.clear();
+                  if (docId == null) {
+                    firestoreService.addTask(
+                      _titleController.text, _descriptionController.text);
+                  } else {               
+                    firestoreService.updateTask(docId, _titleController.text, _descriptionController.text);
+                  }
                   Navigator.pop(context);
                 },
                 child: Text('Salvar'),
@@ -113,8 +119,57 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestoreService.getTasksStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List tasksList = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: tasksList.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot document = tasksList[index];
+                String docId = document.id;
+                Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                String taskTitle = data['title'];
+                String taskDescription = data["description"];
+
+                return Padding(
+                  padding: EdgeInsets.all(16),
+                  child: ListTile(
+                    tileColor: Colors.grey[200],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    title: Text(taskTitle),
+                    subtitle: Text(taskDescription),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            _openModalForm(docId);
+                          },
+                          icon: Icon(Icons.settings),
+                        ), 
+                        IconButton(
+                          onPressed: () {
+                            firestoreService.deleteTask(docId);
+                          },
+                          icon: Icon(Icons.delete),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+            );
+          } else {
+            return Container();
+          }
+        }
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _openModalForm,
+        onPressed: () => _openModalForm(null),
         child: Icon(Icons.add),
       ),
     );
